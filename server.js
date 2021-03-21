e778e0775211db5953c037d339e1c468842d604e
jimblu <jimmy.pezp@gmail.com>
2021-03-21 23:05:09 +0100

    updates

--- server.js
+++ server.js
@@ -1,92 +1,88 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-
-const initializePassport = require('./passport-config');
-
-initializePassport(passport, email => {
-    return users.find(user => user.mail === mail);
-});
-
-const app = express();
-const PORT = 5000;
-
-let users = [];
-
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: 'false'}));
-
-
-app.get('/', (req, res) => {
-    res.render('index', {name: 'jimmy'});
-})
-
-app.get('/login', (req, res) => {
-    res.render('login');
-})
-
-app.post('/login', (req, res) => {
-
-})
-
-app.get('/register', (req, res) => {
-    res.render('register');
-})
-
-app.post('/register', async (req, res) => {
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  const methodOverride = require('method-override')
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(express.static('views'));
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email, 
-            password: hashedPassword
-        });
-        res.redirect('/login');
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
     } catch {
-        res.redirect('/register');
-    }   
-    console.log(users);
-})
-
-
-
-
-
-
-// app.get('/users', (req, res) => {
-//     res.json(users);
-// })
-
-// app.post('/users', async (req, res) => {
-//     try {
-//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//         const user = {name: req.body.name, password: hashedPassword};
-//         users.push(user);
-//         res.status(201).send()
-//     }
-//     catch {
-//         res.status(500).send();
-//     }
-// })
-
-// app.post('/users/login', async (req, res) => {
-//     const user = users.find(x => x.name == req.body.name);
-//     if (user == null) {
-//         res.status(404).send('User not found')
-//     }
-//     try {
-//         if(await bcrypt.compare(req.body.password, user.password)) {
-//             res.send('Success');
-//         } else {
-//             res.send('Not allowed');
-//         }
-//     } catch {
-//         res.status(500).send(); 
-//     }
-// })
-
-app.listen(PORT, () => {
-    console.log(`app listening at http://localhost:${PORT}`)
-})
+      res.redirect('/register')
+    }
+  })
+  
+  app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+  
+  app.listen(5000)
